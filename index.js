d95dadee3530fc58802f0fc7251b17b5bd74f1b6
abhimanyu <abhimanyu.dwibedi@gmail.com>
2022-07-05 17:12:58 +0530

    setting node_env

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(passport.session());
 
 require("./routes/authRoutes")(app);
 require("./routes/billingRoute")(app);
-if (process.env.NODE_ENV !== "production") {
+if (process.env.NODE_ENV === "production") {
   // Express will serve up production assets
   // like our main.js file, or main.css file!
   app.use(express.static("client/build"));
